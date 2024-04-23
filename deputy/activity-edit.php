9babd055058d3d96911c27eb777c054c21747153
unknown <omidshayan.afg@gmail.com>
2024-04-23 15:49:20 +0430

    add activity section in deputy

--- deputy/activity-edit.php
+++ deputy/activity-edit.php
@@ -3,42 +3,38 @@ session_start();
 if (!isset($_SESSION['deputy'])) {
     header('location: ../index.php');
 }
-
-$id = $_GET['id'];
 include_once 'header.php';
 include_once '../connect.php';
-$sql = "SELECT * FROM activity WHERE id = $id";
+$sql = "SELECT * FROM users";
 $result = $connect->query($sql);
-$userInfos = $result->fetch(PDO::FETCH_OBJ);
+$userInfos = $result->fetchAll(PDO::FETCH_ASSOC);
 
+$sql1 = "SELECT * FROM sections";
+$result1 = $connect->query($sql1);
+$sectionInfos = $result1->fetchAll(PDO::FETCH_ASSOC);
+
+$sql2 = "SELECT * FROM `activities` WHERE `id` = ?";
+$result2 = $connect->prepare($sql2);
+$result2->bindValue(1, $_GET['id']);
+$result2->execute();
+$plan = $result2->fetch(PDO::FETCH_OBJ);
+$shamsi_month = jdate('F', $plan->execution_time, '', 'Asia/Kabul', 'fa');
 ?>
+
 <!-- content -->
 <div class="title">
-    <div class="title-text">فعالیت: <?= $userInfos->name ?></div>
+    <div class="title-text">ویرایش فعالیت: <?= $plan->name ?></div>
 </div>
 <br>
+
 <div class="box-content-container">
     <div class="insert">
-        <?php if (isset($_GET['inserted'])) : ?>
+        <?php if (isset($_GET['editing'])) : ?>
             <script>
                 $(document).ready(function() {
                     Swal.fire({
                         icon: 'success',
                         text: 'عملیات با موفقیت انجام شد!',
-                        customClass: {
-                            'swal2-popup': 'black-background'
-                        }
-                    });
-                });
-            </script>
-        <?php endif; ?>
-        <?php if (isset($_GET['error'])) : ?>
-            <script>
-                $(document).ready(function() {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'خطا در ثبت',
-                        text: 'مشکل در ثبت!',
                         customClass: {
                             'swal2-popup': 'black-background'
                         }
@@ -61,31 +57,82 @@ $userInfos = $result->fetch(PDO::FETCH_OBJ);
             </script>
         <?php endif; ?>
 
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                const checkbox = document.getElementById('checkboxInput');
+                const dateTimeInput = document.getElementById('dateTime');
+
+                function toggleDateTimeInput() {
+                    if (checkbox.checked) {
+                        dateTimeInput.removeAttribute('disabled');
+                        dateTimeInput.classList.remove('grayed-out');
+                    } else {
+                        dateTimeInput.setAttribute('disabled', 'disabled');
+                        dateTimeInput.classList.add('grayed-out');
+                    }
+                }
+                toggleDateTimeInput();
+                checkbox.addEventListener('change', toggleDateTimeInput);
+            });
+        </script>
+
 
         <form action="back/activity-edit-check.php" method="POST">
+            <span class="time-title">زمان اجرا: ماه <?= $shamsi_month ?></span>
             <div class="input-group">
                 <div class="input-item">
-                    <div class="lable">عنوان فعالیت </div>
-                    <input type="text" placeholder="عنوان را وارد نمایید..." name="name" value="<?= $userInfos->name ?>" autocomplete="off">
-                    <input type="hidden" placeholder="عنوان را وارد نمایید..." name="id" value="<?= $userInfos->id ?>" autocomplete="off">
+                    <div class="lable ral">
+                        <input type="checkbox" class="checkbox" id="checkboxInput" name="checkbox">
+                        <label for="checkboxInput"><span class="time-change">تغییر زمان اجرا</span></label>
+                    </div>
+                    <input type="hidden" class="form-control d-none dateTime" name="execution_time" autofocus>
+                    <input type="text" class="expire" id="dateTime" value="<?= $plan->execution_time ?>" placeholder="زمان اجرا را وارد نمایید..." disabled autofocus>
                 </div>
                 <div class="input-item">
-                    <div class="lable">تاریخ</div>
-                    <input type="hidden" class="form-control d-none dateTime" name="date" autofocus>
-                    <input type="text" class="expire" id="dateTime" placeholder="زمان اجرا را وارد نمایید..." autofocus>
+                    <div class="lable">بودجه <span class="info">(افغانی)</span> </div>
+                    <input type="text" placeholder="بودجه را وارد نمایید..." name="budget" value="<?= $plan->budget ?>" autocomplete="off">
                 </div>
             </div>
 
-            <div class="lable">توضیحات فعالیت<span class="errors">*</span> </div>
-            <textarea name="content" cols="30" rows="10" placeholder="..."><?= $userInfos->content ?></textarea>
-            <input type="submit" value="ثبت" class="btn btn-color">
-            <a href="activity.php" class="color p5 d-block">برگشت</a>
+            <div class="lable">عنوان پلان <span class="errors">*</span></div>
+            <input type="text" placeholder="عنوان را وارد نمایید..." name="name" value="<?= $plan->name ?>" autocomplete="off">
 
+            <div class="lable">هدف <span class="errors">*</span></div>
+            <input type="text" placeholder="هدف را وارد نمایید..." name="target" value="<?= $plan->target ?>" autocomplete="off">
+
+            <div class="lable">فعالیت <span class="errors">*</span></div>
+            <input type="text" placeholder="فعالیت را وارد نمایید..." name="activiti" value="<?= $plan->activity ?>" autocomplete="off">
+
+            <div class="lable">مسئول اجرا <span class="errors">*</span></div>
+            <select name="implementation">
+                <option disabled>مسئول پیگیری را انتخاب نمایید</option>
+                <?php foreach ($userInfos as $user) :
+                    $name = $user['id'];
+                    $selectedUser = ($name == $plan->implementation) ? 'selected' : '';
+                ?>
+                    <option value="<?= $name ?>" <?= $selectedUser ?>>
+                        <?= $user['name'] ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+
+            <div class="lable">پیگیری توسط <span class="errors">*</span></div>
+            <select name="track">
+                <option disabled>پیگیری را انتخاب نمایید</option>
+                <?php foreach ($sectionInfos as $section) :
+                    $name = $section['id'];
+                    $selectedUser = ($name == $plan->track) ? 'selected' : '';
+                ?>
+                    <option value="<?= $name ?>" <?= $selectedUser ?>>
+                        <?= $section['name'] ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <input type="hidden" name="id" value="<?= $plan->id ?>">
+            <input type="submit" value="ثبت" class="btn btn-color bb">
         </form>
-
     </div>
 </div>
-
 <script type="text/javascript">
     $(document).ready(function() {
         $(".expire").pDatepicker({
---
+++ deputy/back/activity-edit-check.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+if (!isset($_SESSION['deputy'])) {
+    header('location: ../../404.php');
+    exit();
+}
+include_once '../../connect.php';
+
+$name = $_POST['name'];
+$target = $_POST['target'];
+$activiti = $_POST['activiti'];
+if (isset($_POST['implementation'])) {
+    $implementation = $_POST['implementation'];
+}
+if (isset($_POST['track'])) {
+    $track = $_POST['track'];
+}
+$budget = $_POST['budget'];
+$execution_time = $_POST['execution_time'];
+$who_it_added = $_SESSION['user-name'];
+$id = $_POST['id'];
+$checkbox = $_POST['checkbox'];
+
+// inputs validations
+if (empty($name) || empty($target) || empty($activiti) || empty($implementation) || empty($execution_time) || empty($track)) {
+    header("location:../plan-activity.php?empty=10&id=" . $id);
+    exit;
+}
+$execution_time = intval(substr($execution_time, 0, -3));
+
+
+// query update
+$sql = "UPDATE `activities` SET 
+        `name` = ?, 
+        `target` = ?, 
+        `activity` = ?, 
+        `implementation` = ?, 
+        `track` = ?, 
+        `budget` = ?";
+if (isset($checkbox)) {
+    $sql .= ", `execution_time` = ?";
+}
+$sql .= " WHERE `id` = ?";
+$result = $connect->prepare($sql);
+
+$result->bindValue(1, $name);
+$result->bindValue(2, $target);
+$result->bindValue(3, $activiti);
+$result->bindValue(4, $implementation);
+$result->bindValue(5, $track);
+$result->bindValue(6, $budget);
+if (isset($checkbox)) {
+    $result->bindValue(7, $execution_time);
+    $result->bindValue(8, $id);
+} else {
+    $result->bindValue(7, $id);
+}
+
+
+if ($result->execute()) {
+    header("location:../activity-edit.php?editing=20&id=" . $id);
+} else {
+    header("location:../activity-edit.php?error=30&id=" . $id);
+}
